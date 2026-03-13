2cc050b1c1b6cf3d971e41ba8c343644d44786b7
Yusuke Shibata <yusukeshib@gmail.com>
2026-03-12 22:09:49 -0400

    worktree

--- private_dot_config/zsh/init.zsh
+++ private_dot_config/zsh/init.zsh
@@ -11,6 +11,7 @@ export LC_ALL=en_US.UTF-8
 export FZF_DEFAULT_COMMAND='fd --type f -i'
 export PATH="$HOME/.local/bin:$HOME/.cargo/bin:$PATH"
 export BOX_DEFAULT_CMD="claude"
+export BOX_STRATEGY="worktree"
 
 if type "nixy" > /dev/null; then
   eval "$(nixy config zsh)"
